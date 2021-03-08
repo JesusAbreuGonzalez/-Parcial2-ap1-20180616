adfe633066e7c639e18206642d8a56baa32dbacc
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-03-08 19:14:51 -0400

    Agregando metodo Existe a TiposTareaBLL

---
+++ -Parcial2-ap1-20180616/BLL/TiposTareaBLL.cs
@@ -0,0 +1,33 @@
+﻿using _Parcial2_ap1_20180616.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Parcial2_ap1_20180616.BLL
+{
+    public class TiposTareaBLL
+    {
+        public static bool Existe(string descripcion)
+        {
+            bool encontrado = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Proyectos.Any(e => e.Descripcion.ToLower() == descripcion.ToLower());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+    }
+}
