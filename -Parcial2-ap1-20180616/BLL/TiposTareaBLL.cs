93167a60d9ecbfedbd51f3341f592ba5915f1bc1
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-03-08 19:22:37 -0400

    Agregando metodo Modificar a TiposTareBLL

--- -Parcial2-ap1-20180616/BLL/TiposTareaBLL.cs
+++ -Parcial2-ap1-20180616/BLL/TiposTareaBLL.cs
@@ -1,5 +1,6 @@
 ﻿using _Parcial2_ap1_20180616.DAL;
 using _Parcial2_ap1_20180616.Entidades;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,5 +54,26 @@ namespace _Parcial2_ap1_20180616.BLL
             return paso;
         }
 
+        private static bool Modificar(Proyectos proyectos)
+        {
+            bool paso = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(proyectos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
     }
 }
