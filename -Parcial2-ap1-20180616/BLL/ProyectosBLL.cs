159f41f0dea3536e27cb066dfe98dc1d3da124e6
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-03-08 19:10:22 -0400

    Agregando ProyectosBLL

---
+++ -Parcial2-ap1-20180616/BLL/ProyectosBLL.cs
@@ -0,0 +1,160 @@
+﻿using _Parcial2_ap1_20180616.DAL;
+using _Parcial2_ap1_20180616.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Parcial2_ap1_20180616.BLL
+{
+    public class ProyectosBLL
+    {
+        public static bool Existe(string descripcion)
+        {
+            bool encontrado = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                //Comparamos ambos textos para poder validar que no se repitan descripciones
+                encontrado = contexto.Proyectos.Any(e => e.Descripcion.ToLower() == descripcion.ToLower());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        private static bool Insertar(Proyectos proyectos)
+        {
+            bool paso = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                contexto.Proyectos.Add(proyectos);
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        private static bool Modificar(Proyectos proyectos)
+        {
+            bool paso = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                contexto.Database.ExecuteSqlRaw($"Delete from ProyectosDetalle where proyectoId = {proyectos.ProyectoId}");
+                foreach (var anterior in proyectos.DetalleProyecto)
+                {
+                    contexto.Entry(anterior).State = EntityState.Added;
+                }
+                contexto.Entry(proyectos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Guardar(Proyectos proyectos)
+        {
+            if (!Existe(proyectos.Descripcion))
+                return Insertar(proyectos);
+            else
+                return Modificar(proyectos);
+        }
+
+        public static Proyectos Buscar(int id)
+        {
+            var contexto = new Contexto();
+            var proyecto = new Proyectos();
+
+            try
+            {
+                proyecto = contexto.Proyectos.Include(x => x.DetalleProyecto).Where(p => p.ProyectoId == id).SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return proyecto;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                var eliminarProyecto = contexto.Proyectos.Find(id);
+                contexto.Entry(eliminarProyecto).State = EntityState.Deleted;
+
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static List<Proyectos> GetList(Expression<Func<Proyectos, bool>> criterio)
+        {
+            List<Proyectos> lista = new List<Proyectos>();
+            var contexto = new Contexto();
+            
+            try
+            {
+                lista = contexto.Proyectos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
